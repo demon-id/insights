99ce6240d0140ed94dbd1b6ef30876606824fdae
demon_id <demon_id@itechs.com.ua>
2015-10-14 17:56:43 +0300

    +

---
+++ core/CoreApiClient.php
@@ -0,0 +1,69 @@
+<?php
+namespace insights\api\core;
+
+use GuzzleHttp\Client AS HTTPClient;
+use yii\base\Component;
+use yii\helpers\ArrayHelper;
+
+class CoreApiClient extends Component {
+
+	public $apiUrl;
+
+	public $apiKey;
+
+	protected $HTTPClient;
+
+	protected $methodParams = [
+		'not-in-crawler-sites' => [
+			'url' => 'sites/not-in-crawler-sites',
+			'type' => 'get'
+		],
+		'mark-site-as-in-crawler' => [
+			'url' => 'sites/mark-as-in-crawler',
+			'type' => 'put'
+		],
+	];
+
+	public function __construct() {
+		$this->HTTPClient = new HTTPClient;
+	}
+
+	protected function sendRequest($name, $params=[], $headers=[])
+	{
+		$headers = ArrayHelper::merge($headers, [
+			'api_key' => $this->apiKey
+		]);
+
+		$request_params = $this->getRequestParams($name);
+		$request_type = $request_params['type'];
+		$request_url = $this->apiUrl.$request_params['url'];
+		$response = $this->HTTPClient->$request_type($request_url, [
+			'headers' => $headers,
+			'body' => $params
+		]);
+
+		$answer = $response->json();
+
+		return $answer;
+	}
+
+	protected function getRequestParams($name)
+	{
+		if (empty($this->methodParams[$name])) {
+			throw new \BadMethodCallException('Api method is undefined.');
+		}
+
+		return $this->methodParams[$name];
+	}
+
+
+	public function getNotInCrawlerSites($limit=10)
+	{
+		return $this->sendRequest('not-in-crawler-sites', ['limit'=>$limit]);
+	}
+
+	public function markSiteAsInCrawler($core_site_id)
+	{
+		return $this->sendRequest('mark-site-as-in-crawler', ['site_id'=>$core_site_id]);
+	}
+}
---
+++ core/Exception.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace insights\api\core;
+
+class Exception extends \Exception {
+
+	private $answer;
+
+	public function __construct($message, $code, $previous, $answer = null) {
+		parent::__construct($message, $code, $previous);
+		$this->answer = $answer;
+	}
+
+	public final function getAnswer(){
+		return $this->answer;
+	}
+}
