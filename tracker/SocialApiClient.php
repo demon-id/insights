1b73a6a431ede88a85e889806b8b9d6b9d5a4018
svishnov <svishnov@gmail.com>
2016-03-30 18:01:44 +0300

    add socialApiClient

---
+++ tracker/SocialApiClient.php
@@ -0,0 +1,118 @@
+<?php
+namespace insights\api\tracker;
+
+use GuzzleHttp\Client AS HTTPClient;
+use GuzzleHttp\Post\PostFile;
+use yii\base\Component;
+use yii\helpers\ArrayHelper;
+use common\components\Log;
+
+class SocialApiClient extends Component
+{
+    /**
+     * @const prefix CURLOPT_
+     */
+    const CONNECTTIMEOUT = 5;
+    const TIMEOUT = 5;
+
+    public $apiUrl;
+
+    public $apiKey;
+
+    protected $HTTPClient;
+
+    protected $methodParams = [
+        'get-social-pages' => [
+            'url' => 'get/social-pages',
+            'type' => 'get'
+        ],
+    ];
+
+    public function __construct() {
+        $this->HTTPClient = new HTTPClient;
+    }
+
+    protected function sendRequest($name, $query_params=[], $body_params=[], $headers=[])
+    {
+        $headers = ArrayHelper::merge($headers, [
+            'api-key' => $this->apiKey
+        ]);
+
+        $request_params = $this->getRequestParams($name);
+        $request_type = $request_params['type'];
+        $request_url = $this->apiUrl.$request_params['url'];
+
+        $options = [
+            'headers'=>$headers
+        ];
+
+        $options['query'] = $query_params;
+        $options['exceptions'] = false;
+        $options['connect_timeout'] = self::CONNECTTIMEOUT;
+        $options['timeout'] = self::TIMEOUT;
+
+        if (!empty($headers['Content-type']) && $headers['Content-type'] == 'multipart/form-data') {
+            $options['body'] = $body_params;
+        } else {
+            $options['json'] = $body_params;
+        }
+
+        $response = null;
+        try {
+            $response = $this->HTTPClient->$request_type($request_url, $options);
+
+            $answer = $response->json();
+
+            return $answer;
+
+        } catch(\GuzzleHttp\Exception\BadResponseException $e) {
+
+            $this->logExceptions($request_url, $response, $e);
+
+            return false;
+        } catch(\GuzzleHttp\Exception\ParseException $e) {
+
+            $this->logExceptions($request_url, $response, $e);
+
+            return false;
+        } catch(\GuzzleHttp\Exception\ConnectException $e) {
+
+            $this->logExceptions($request_url, $response, $e);
+
+            return false;
+        }
+
+    }
+
+    protected function logExceptions($request_url, $response, $e)
+    {
+        Log::add(
+            'Url:'.$request_url."\n".
+            'Message: '.$e->getMessage()."\n".
+            'Response: '.(($response) ? $response->getBody() : '')."\n",
+            'api-http-errors',
+            \Yii::getAlias('@runtime').'/logs'
+        );
+    }
+
+    protected function getRequestParams($name)
+    {
+        if (empty($this->methodParams[$name])) {
+            throw new \BadMethodCallException('Api method is undefined.');
+        }
+
+        return $this->methodParams[$name];
+    }
+
+    /**
+     * @param $form_id
+     * @param int $page
+     * @return bool
+     */
+    public function getSocialPages($email)
+    {
+        return $this->sendRequest('get-social-pages', [
+            'email' => $email,
+        ]);
+    }
+}
